7926b9cae763ade198f3fc6d6c64bf6c4a939504
root <root@ip-172-31-35-109.us-east-2.compute.internal>
2020-09-05 17:56:38 +0000

    latest with Ubuntu

--- ec2/vars.tf
+++ ec2/vars.tf
@@ -3,7 +3,10 @@ variable "ec2_count" {
 }
 
 variable "ami_id" {
-  default = "ami-07c8bc5c1ce9598c3"
+  ###LINUX AMI 
+  #default = "ami-07c8bc5c1ce9598c3"
+  ##UBUNTU
+  default = "ami-0010d386b82bc06f0"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
@@ -18,3 +21,7 @@ variable "INSTANCE_USERNAME" {
   default = "ec-user"
 }
 
+variable "ports" {
+  type    = list(number)
+  default = [22, 80, 8080, 6443, 10250]
+}
